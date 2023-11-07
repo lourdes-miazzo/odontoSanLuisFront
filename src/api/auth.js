f05a8a7d1792bd3736e534fbf10bf0a519de8cfa
lourdes-miazzo <108072073+lourdes-miazzo@users.noreply.github.com>
2023-11-07 14:33:30 -0300

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API = "http://localhost:8081/api"
+const API = "https://circuloodontologico.onrender.com/api"
 
 export const contactRequest = async (contact) => {
     try {
